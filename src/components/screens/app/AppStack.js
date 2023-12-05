40ecea30b80c3054cb389176f859c76be91b156e
Ali Murtaza <126866806+murtaza3304@users.noreply.github.com>
2023-12-05 18:35:55 +0500

    testing

--- src/components/screens/app/AppStack.js
+++ src/components/screens/app/AppStack.js
@@ -41,6 +41,9 @@ const AppStack = () => {
       <Stack.Screen name="PendingComponent" component={PendingComponent} />
 
       {/* Searching */}
+      {/* Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. */}
+
+
       <Stack.Screen name="BottomSearch2" component={BottomSearch2} />
       <Stack.Screen name="BottomSearch3" component={BottomSearch3} />
       <Stack.Screen name="SearchHistory" component={SearchHistory} />
